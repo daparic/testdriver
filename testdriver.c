f8f3a9e2f912d4d383a3cb748883d4e0e55b103a
Your Name <you@example.com>
2023-01-17 13:43:01 +0800

    Add .clang-format

--- testdriver.c
+++ testdriver.c
@@ -1,18 +1,18 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
 #include <asm/uaccess.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 #define DEVICE_NAME "testdriver"
 #define EXAMPLE_MSG "My precious!\n"
-#define MSG_BUFFER_LEN 13 
+#define MSG_BUFFER_LEN 13
 
 struct Dog {
-    int n;
-    unsigned char ch;
+        int n;
+        unsigned char ch;
 };
 
 static int device_open(struct inode *, struct file *);
@@ -22,20 +22,22 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 static int major_num;
 static int device_open_count = 0;
 static char msg_buffer[MSG_BUFFER_LEN];
-static char *msg_ptr; 
+static char *msg_ptr;
 static int counter = 0;
 
-static struct file_operations file_ops = { .read = device_read, .write = device_write,
-    .open = device_open, .release = device_release
-}; 
+static struct file_operations file_ops = {.read = device_read,
+                                          .write = device_write,
+                                          .open = device_open,
+                                          .release = device_release};
 
-static ssize_t device_read(struct file *flip, char *buffer, size_t len, loff_t *offset)
-{
+static ssize_t device_read(struct file *flip, char *buffer, size_t len,
+                           loff_t *offset) {
     struct Dog *pDog = NULL;
     int bytes_read = 0;
     printk(KERN_ALERT "** READ OPS detected[%d] ***\n", counter);
     counter++;
-    if (*msg_ptr == 0) msg_ptr = msg_buffer;
+    if (*msg_ptr == 0)
+        msg_ptr = msg_buffer;
     while (len && *msg_ptr) {
         put_user(*(msg_ptr++), buffer++);
         len--;
@@ -51,31 +53,29 @@ static ssize_t device_read(struct file *flip, char *buffer, size_t len, loff_t *
     }
 
     return bytes_read;
-} 
+}
 
-static ssize_t device_write(struct file *flip, const char *buffer, size_t len, loff_t *offset)
-{
+static ssize_t device_write(struct file *flip, const char *buffer, size_t len,
+                            loff_t *offset) {
     printk(KERN_ALERT "This operation is not supported.\n");
     return -EINVAL;
-} 
+}
 
-static int device_open(struct inode *inode, struct file *file)
-{
-    if (device_open_count) return -EBUSY;
+static int device_open(struct inode *inode, struct file *file) {
+    if (device_open_count)
+        return -EBUSY;
     device_open_count++;
     try_module_get(THIS_MODULE);
     return 0;
-} 
+}
 
-static int device_release(struct inode *inode, struct file *file)
-{
+static int device_release(struct inode *inode, struct file *file) {
     device_open_count--;
     module_put(THIS_MODULE);
     return 0;
 }
 
-static int __init testdriver_init(void)
-{
+static int __init testdriver_init(void) {
     strncpy(msg_buffer, EXAMPLE_MSG, MSG_BUFFER_LEN);
     msg_ptr = msg_buffer;
     major_num = register_chrdev(0, "testdriver", &file_ops);
@@ -83,16 +83,17 @@ static int __init testdriver_init(void)
         printk(KERN_ALERT "Could not register device : % d\n", major_num);
         return major_num;
     } else {
-        printk(KERN_INFO "testdriver module loaded with device major number % d\n", major_num);
+        printk(KERN_INFO
+               "testdriver module loaded with device major number % d\n",
+               major_num);
         return 0;
     }
 }
 
-static void __exit testdriver_exit(void)
-{
+static void __exit testdriver_exit(void) {
     unregister_chrdev(major_num, DEVICE_NAME);
     printk(KERN_INFO "Goodbye, testdriver !\n");
-} 
+}
 
 module_init(testdriver_init);
 module_exit(testdriver_exit);
